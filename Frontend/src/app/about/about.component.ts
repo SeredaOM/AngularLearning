7181d682faed4fff30c192661051b696b0d68fca
Budda <seredaom@gmail.com>
2021-05-11 23:23:31 -0400

    DisplayVersion

--- Frontend/src/app/about/about.component.ts
+++ Frontend/src/app/about/about.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+//import { version } from '../../../package.json';
+//import { version2 } from '../../package.json';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
+  public production = environment.production;
+  public version = environment.version;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
