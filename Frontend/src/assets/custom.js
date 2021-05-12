80352fbb59061d4ce33d88293fa74a766418a5c6
Budda <seredaom@gmail.com>
2021-05-11 22:01:42 -0400

    Removed custom js code that caused tests in pingball component to fail

--- Frontend/src/assets/custom.js
+++
@@ -1,9 +0,0 @@
-(function custom_auto() {
-    console.log("custom_auto: this is called automatically when component imports the file"); 
-})()
-
-function custom_test() {
-    console.log("custom_test: Hello!!!"); 
-}
-
-window.initMethod = function() { console.log("custom: Hello!!!"); }
