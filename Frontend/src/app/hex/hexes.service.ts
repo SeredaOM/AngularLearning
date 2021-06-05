c8e7bbfbb274d94fd0394960bbdd96fbd3266ea1
Budda <seredaom@gmail.com>
2021-06-05 15:35:54 -0400

    Switched HexesService to use host from the ApiService
    As a result, all services will use API based on the environment

--- Frontend/src/app/hex/hexes.service.ts
+++ Frontend/src/app/hex/hexes.service.ts
@@ -2,16 +2,13 @@ import { Injectable, isDevMode } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { ITile } from './itile';
+import { ApiService } from '../api.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HexesService {
-  constructor(private httpClient: HttpClient) {}
-
-  private static getHost() {
-    return isDevMode() ? 'https://localhost:44362' : 'http://localhost:83';
-  }
+  constructor(private httpClient: HttpClient, private apiService: ApiService) {}
 
   private static httpOptions = {
     headers: new HttpHeaders({
@@ -20,12 +17,12 @@ export class HexesService {
   };
 
   public getMap(mapId: number) {
-    return this.httpClient.get(HexesService.getHost() + '/hexes?mapId=' + mapId);
+    return this.httpClient.get(ApiService.getHost() + '/hexes?mapId=' + mapId);
   }
 
   public postTile(tile: ITile) {
     return this.httpClient.post(
-      HexesService.getHost() + '/hexes',
+      ApiService.getHost() + '/hexes',
       tile.serialize(),
       HexesService.httpOptions
     );
