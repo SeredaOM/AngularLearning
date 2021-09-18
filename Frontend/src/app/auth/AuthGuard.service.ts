0ffea7a81fe75da1c6cd1990c7b7247a11cf1774
Budda <seredaom@gmail.com>
2021-09-18 16:58:11 -0400

    Replace authorization service for the AuthGuard
    SocialAuthService (client-side) is replaced with AuthService (backend). This is required to implement access validation based on backend feedback, not client one.
    The reason for this is that user must be registerred to pass the guard, not just logged in to a 3rd party social system.

--- Frontend/src/app/auth/AuthGuard.service.ts
+++ Frontend/src/app/auth/AuthGuard.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
+import { SocialAuthService, SocialUser } from 'angularx-social-login';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
-
-import { SocialAuthService, SocialUser } from 'angularx-social-login';
 
 import { Role } from './role';
 import { AuthService } from './AuthService';
@@ -12,27 +11,23 @@ import { AuthService } from './AuthService';
   providedIn: 'root',
 })
 export class AuthGuardService implements CanActivate {
-  constructor(private router: Router, private socialAuthService: SocialAuthService) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return this.socialAuthService.authState.pipe(
-      map((user: SocialUser) => {
-        if (user == null) return false;
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const loggedIn = this.authService.isLoggedIn();
+    if (!loggedIn) {
+      this.router.navigate(['login']);
+      console.log(`Can't access ${route.url}`);
+      return false;
+    }
 
-        const usersRole = localStorage.getItem(AuthService.Role);
+    const usersRole = localStorage.getItem(AuthService.Role);
 
-        if (route.data.role) {
-          if (route.data.role > Role[usersRole]) {
-            return false;
-          }
-        }
-        return true;
-      }),
-      tap((isAuthorized: boolean) => {
-        if (!isAuthorized) {
-          this.router.navigate(['login']);
-        }
-      })
-    );
+    if (route.data.role) {
+      if (route.data.role > Role[usersRole]) {
+        return false;
+      }
+    }
+    return true;
   }
 }
