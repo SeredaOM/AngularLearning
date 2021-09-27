8b2e14f494021dee21285592d1126b068508bb71
Budda <seredaom@gmail.com>
2021-09-26 23:23:08 -0400

    Bug-fix: display 'Admin' button to admins only

--- Frontend/src/app/app.component.spec.ts
+++ Frontend/src/app/app.component.spec.ts
@@ -10,6 +10,7 @@ import { Subject } from 'rxjs';
 
 import { AppComponent } from './app.component';
 import { AuthService } from './auth/AuthService';
+import { PlayerModel } from './Models/PlayerModel';
 
 describe('AppComponent', () => {
   let loader: HarnessLoader;
@@ -22,6 +23,9 @@ describe('AppComponent', () => {
 
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
+
+  const playerAdmin: PlayerModel = new PlayerModel(1, 'nick', 'email', 'first', 'last', 'admin');
+  const playerRegular: PlayerModel = new PlayerModel(1, 'nick', 'email', 'first', 'last', null);
 
   beforeEach(async () => {
     authClientSpy = jasmine.createSpyObj('AuthService', ['isLoggedIn'], { loggedIn: authClientLoggedInSubject });
@@ -94,6 +98,31 @@ describe('AppComponent', () => {
       const mapsButtons = await loader.getAllHarnesses(MatButtonHarness.with({ text: 'Maps' }));
       expect(mapsButtons.length).toBe(1);
       expect(mapsButtons[0]).toBeTruthy();
+    })
+  );
+
+  it(
+    'should NOT display "admin" button to non-admins',
+    waitForAsync(async () => {
+      authClientSpy.isLoggedIn.and.returnValue(true);
+      spyOn(PlayerModel, 'getPlayerFromStore').and.returnValue(playerRegular);
+      createComponent();
+
+      const adminButtons = await loader.getAllHarnesses(MatButtonHarness.with({ text: 'Admin' }));
+      expect(adminButtons.length).toBe(0);
+    })
+  );
+
+  it(
+    'should display "admin" button to admins',
+    waitForAsync(async () => {
+      authClientSpy.isLoggedIn.and.returnValue(true);
+      spyOn(PlayerModel, 'getPlayerFromStore').and.returnValue(playerAdmin);
+      createComponent();
+
+      const adminButtons = await loader.getAllHarnesses(MatButtonHarness.with({ text: 'Admin' }));
+      expect(adminButtons.length).toBe(1);
+      expect(adminButtons[0]).toBeTruthy();
     })
   );
 });
--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 import { AuthService } from './auth/AuthService';
+import { Role } from './Models/Auth/role';
+import { PlayerModel } from './Models/PlayerModel';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +13,25 @@ export class AppComponent {
   title = 'angular-example';
 
   loggedIn: Boolean = false;
+  isAdmin: Boolean = false;
 
   constructor(private authService: AuthService) {
     this.authService.loggedIn.subscribe((val) => {
-      this.loggedIn = this.authService.isLoggedIn();
+      this.pullLoginData();
     });
   }
 
   ngOnInit() {
+    this.pullLoginData();
+  }
+
+  private pullLoginData() {
     this.loggedIn = this.authService.isLoggedIn();
+    if (this.loggedIn) {
+      const player: PlayerModel = PlayerModel.getPlayerFromStore();
+      this.isAdmin = Role[player.role] == Role.admin;
+    } else {
+      this.isAdmin = false;
+    }
   }
 }
